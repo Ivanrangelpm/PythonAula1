fa50dbfce6b19dc8824fbf5e85989cab2da0114c
Ivanrangelpm <rm551005@fiap.com.br>
2023-03-13 11:16:57 -0300

    Complemento e else

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,8 +1,11 @@
 print('*-* Verifica idade *-*')
 
-idade = 19
+idade = int (input('Digite sua idade: '))
 
 if idade >= 18:
     print('Maior de idade')
+    print('Voce já pode ser preso')
+    print('Voce pode ter um CNH')
 else: 
     print('Menor de idade')
+    print('Batatinha quando nasce esparrama pelo chão')
